670a8e8392bf0f09e196a744f9d616791614e24f
M. Hanif Azhary <hanif@azhary.dev>
2022-06-02 18:23:41 +0000

    Initial commit: creating pyramid in go

---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var rows int = 5
+	var k int
+
+	fmt.Printf("We will be printing pyramid with %d rows\n", rows)
+
+	for i := 1; i <= rows; i++ {
+		k = 0
+		for space := 1; space <= rows-i; space++ {
+			fmt.Print("  ")
+		}
+		for {
+			fmt.Print("* ")
+			k++
+			if k == 2*i-1 {
+				break
+			}
+		}
+		fmt.Println("")
+	}
+}
