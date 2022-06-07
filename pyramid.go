2fabcb9016ae11db2230d4a228a228be4078f058
M. Hanif Azhary <hanif@azhary.dev>
2022-06-07 15:55:04 +0000

    refactor: optimize pyramid printing function, make it more dynamic

--- pyramid.go
+++ pyramid.go
@@ -2,22 +2,19 @@ package main
 
 import (
 	"fmt"
+	"strings"
 )
 
 func pyramid(rows int) {
+	var stack = make([]string, 2*rows-1)
+	for i := range stack {
+		stack[i] = " "
+	}
 	var k int
-	for i := 1; i <= rows; i++ {
-		k = 0
-		for space := 1; space <= rows-i; space++ {
-			fmt.Print("  ")
-		}
-		for {
-			fmt.Print("* ")
-			k++
-			if k == 2*i-1 {
-				break
-			}
-		}
-		fmt.Println("")
+	for i := rows - 1; i >= 0; i-- {
+		stack[i] = "*"
+		stack[i+k] = "*"
+		fmt.Println(strings.Join(stack, " "))
+		k += 2
 	}
 }
